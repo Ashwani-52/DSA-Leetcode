821079f3b4fa97d2aa0f4787faced85cb3fa535e
Ashwani Kumar <164582007+Ashwani-52@users.noreply.github.com>
2026-08-07 17:14:30 +0530

    Time: 1 ms (99.96%), Space: 77.3 MB (32.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,14 +4,22 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
 
         for(int i=0; i<nums.length; i++){
+            //step 1: Create Sum
             sum += nums[i];
-            if(maxSum < sum){
-                maxSum = sum;
-            }
+
+            //Step 2: maxSum update krna h
+            // if(maxSum < sum){
+            //     maxSum = sum;
+            // }
+
+            maxSum = Math.max(maxSum, sum);
+
+            //step 3: sum check krna h, Negative h ya nhi
             if(sum < 0){
                 sum = 0;
             }
         }
+        //return max value
         return maxSum;
     }
 }

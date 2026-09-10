e2b767d9d890cb8a34398f9d6bd00ff2c48a4900
Ashwani Kumar <164582007+Ashwani-52@users.noreply.github.com>
2026-09-10 11:39:26 +0530

    Time: 2 ms (93.2%), Space: 72.3 MB (16.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -5,15 +5,13 @@ class Solution {
         ans[0] = 1;
         int suffix = 1;
 
-        for(int i=1; i<n; i++){
+        for(int i = 1; i<n; i++){
             ans[i] = ans[i-1] * nums[i-1];
         }
-
-        for(int i = n-2; i>=0; i--){
+        for(int i=n-2; i>=0; i--){
             suffix *= nums[i+1];
             ans[i] *= suffix;
         }
-        
         return ans;
     }
 }
